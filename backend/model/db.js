bcb4101e084c421c08892c717232a8f4ba441cf1
Manon Philippot <manon.philippot@gmail.com>
2018-12-15 14:01:48 +0100

    init MongoDB with mongoose

--- backend/model/db.js
+++ backend/model/db.js
@@ -10,23 +10,17 @@ mongoose.connect(dbURL, options);
 
 // CONNECTION EVENTS
 // when successfully connected
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + dbURL);
-});
+mongoose.connection.on('connected', () => console.log('Mongoose default connection open to ' + dbURL));
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-});
+mongoose.connection.on('error', (err) => console.log('Mongoose default connection error: ' + err));
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-});
+mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'));
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
     console.log('Mongoose default connection disconnected through app termination');
     process.exit(0);
   });
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../model/db');
 
 router.get('/', (req, res) => res.send('Hello World!'));
 
