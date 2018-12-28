b792816a8c65d21ff0e9f6438738fb7102da89fe
Manon Philippot <manon.philippot@gmail.com>
2018-12-29 00:56:40 +0100

    add authentication for API

--- backend/routes/routes_authentication.js
+++ backend/routes/routes_authentication.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrlAuth = require('../controllers/ctrl_authentication');
 
+const jwt = require('express-jwt');
+const auth = jwt({
+  secret: 'THE_SECRET',
+  userProperty: 'payload'
+});
+
 // (POST) /api/register - to handle new users registering
 router.post('/register', ctrlAuth.register);
 
@@ -9,6 +15,6 @@ router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
 
 // (GET) /api/profile/USERID - to return profile xetails when given a USERID
-router.get('/profile', ctrlAuth.getProfile)
+router.get('/profile', auth, ctrlAuth.getProfile)
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,21 @@ require('./model/db');
 // Bring the passport config, and initialize
 require('./config/passport');
 
-// init parameters
+// Init parameters
 app.use(cors());
 app.use(bodyParser.json());
 app.use(expressValidator())
 app.use(passport.initialize());
 
-// import routes
+// Import routes
 let routesAuthentication = require('./routes/routes_authentication');
 app.use('/api/', routesAuthentication);
+
+// Catch 404 and forward to error handler
+app.get('*', function(req, res, next) {
+  let err = new Error('Page Not Found');
+  err.statusCode = 404;
+  next(err);
+});
 
 app.listen(4000, () => console.log(`Express server running on port 4000`));
