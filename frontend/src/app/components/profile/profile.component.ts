c4f0c7a8656710ad41ec41818b005cdd78835a29
Manon Philippot <manon.philippot@gmail.com>
2018-12-29 17:08:32 +0100

    frontend profile page

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService, UserDetails } from '../../services/authentication/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userDetails: UserDetails;
+
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit() {
+    this.auth.getProfile().subscribe(user => {
+      this.userDetails = user;
+    }, (err) => {
+      console.error(err);
+    });
   }
 
 }
