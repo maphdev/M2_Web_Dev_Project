225e8528342412d5804ec5497366a8a66e020452
Manon Philippot <manon.philippot@gmail.com>
2019-01-13 22:20:53 +0100

    Deal with no background images

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: HomeComponent
+        component: ListsmoviesComponent
       },
       {
         path: 'discover/:category/:page',
