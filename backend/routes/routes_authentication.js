c5278a43e534d64995c2819ec8c30e07975f7505
Manon Philippot <manon.philippot@gmail.com>
2019-01-16 20:13:16 +0100

    Handle 405 : method not allowed

--- backend/routes/routes_authentication.js
+++ backend/routes/routes_authentication.js
@@ -12,4 +12,11 @@ router.post('/login', ctrlAuth.login);
 // (GET) /api/profile/USERID - to return profile xetails when given a USERID
 router.get('/user/profile', auth, ctrlAuth.getProfile);
 
+// Handle 405 error : method not allowed
+const handler = require('../config/handler.js');
+
+router.all('/register', handler(['POST']));
+router.all('/login', handler(['POST']));
+router.all('/user/profile', handler(['GET']));
+
 module.exports = router;
