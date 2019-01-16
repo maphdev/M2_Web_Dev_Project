802c168d3fdce352581b0058bd232a996d0a4431
firstein <alienor.brabant@gmail.com>
2019-01-16 19:12:25 +0100

    correction bug from delete in a movielist

--- backend/controllers/ctrl_watchlist.js
+++ backend/controllers/ctrl_watchlist.js
@@ -47,17 +47,17 @@ module.exports.deleteMovieToWatchlist = function (req, res) {
     res.status(401).send({success: false, message: "UnauthorizedError: private profile"});
   } else {
     User.findById(req.payload._id)
-      .exec(function (err, user) {
-        if (!user.watchlist.includes(parseInt(req.params.movie_id))) {
-          return res.status(404).send({success: false, message: "Movie not in watchlist"});
-        }
-        User.findByIdAndUpdate(req.payload._id,  {$pull: {watchlist: parseInt(req.params.movie_id)}}, {upsert: true})
-          .exec(function(err, user) {
-            if (err) {
-              return res.status(500).send({success: false, message: err});
-            }
-            res.status(200).send({success: true, message: "Movie deleted from watchlist!"});
-          });
+        .exec(function (err, user) {
+          if (!user.watchlist.includes(parseInt(req.params.movie_id))) {
+            return res.status(404).send({success: false, message: "Movie not in watchlist"});
+          }
+          User.findByIdAndUpdate(req.payload._id,  {$pull: {watchlist: parseInt(req.params.movie_id)}}, {upsert: true})
+              .exec(function(err, user) {
+                if (err) {
+                  return res.status(500).send({success: false, message: err});
+                }
+                res.status(200).send({success: true, message: "Movie deleted from watchlist!"});
+              });
         });
   }
 };
@@ -107,21 +107,18 @@ module.exports.deleteMovieToFavoritelist = function (req, res) {
   if (!req.payload._id) {
     res.status(401).send({success: false, message: "UnauthorizedError: private profile"});
   } else {
-    let exist = User.findById(req.payload._id)
-                    .exec(function (err, user) {
-                      return user.favoritelist.includes(parseInt(req.params.movie_id))
-                    });
-    if (!exist) {
-      return res.status(404).send({success: false, message: "Movie not in favoritelist"});
-    }
-    User.findByIdAndUpdate(req.payload._id,  {$pull: {favoritelist: parseInt(req.params.movie_id)}}, {upsert: true})
-        .exec(function(err, user) {
-          if (err) {
-            return res.status(500).send({success: false, message: err});
+    User.findById(req.payload._id)
+        .exec(function (err, user) {
+          if (!user.favoritelist.includes(parseInt(req.params.movie_id))) {
+            return res.status(404).send({success: false, message: "Movie not in favoritelist"});
           }
-          console.log(user);
-          console.log(req.params.movie_id);
-          res.status(200).send({success: true, message: "Movie deleted from favoritelist!"});
+          User.findByIdAndUpdate(req.payload._id,  {$pull: {favoritelist: parseInt(req.params.movie_id)}}, {upsert: true})
+              .exec(function(err, user) {
+                if (err) {
+                  return res.status(500).send({success: false, message: err});
+                }
+                res.status(200).send({success: true, message: "Movie deleted from favoritelist!"});
+              });
         });
   }
 };
@@ -171,19 +168,18 @@ module.exports.deleteMovieToSeenlist = function (req, res) {
   if (!req.payload._id) {
     res.status(401).send({success: false, message: "UnauthorizedError: private profile"});
   } else {
-    let exist = User.findById(req.payload._id)
-                    .exec(function (err, user) {
-                      return user.seenlist.includes(parseInt(req.params.movie_id))
-                    });
-    if (!exist) {
-      return res.status(404).send({success: false, message: "Movie not in seenlist"});
-    }
-    User.findByIdAndUpdate(req.payload._id,  {$pull: {seenlist: parseInt(req.params.movie_id)}}, {upsert: true})
-        .exec(function(err, user) {
-          if (err) {
-            return res.status(500).send({success: false, message: err});
+    User.findById(req.payload._id)
+        .exec(function (err, user) {
+          if (!user.seenlist.includes(parseInt(req.params.movie_id))) {
+            return res.status(404).send({success: false, message: "Movie not in seenlist"});
           }
-          res.status(200).send({success: true, message: "Movie deleted from favoritelist!"});
+          User.findByIdAndUpdate(req.payload._id,  {$pull: {seenlist: parseInt(req.params.movie_id)}}, {upsert: true})
+              .exec(function(err, user) {
+                if (err) {
+                  return res.status(500).send({success: false, message: err});
+                }
+                res.status(200).send({success: true, message: "Movie deleted from seenlist!"});
+              });
         });
   }
 };
